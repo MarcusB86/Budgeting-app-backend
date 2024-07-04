f42e8b192bdc2c1f193813b9b8a53088ea00be8b
MarcusB86 <marcusb08@gmail.com>
2024-07-04 15:23:55 -0400

    Create Destroy Update complete

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -16,5 +16,35 @@ transaction.get("/:id", (req, res) => {
     }
 })
 
+transaction.post("/", (req,res) => {
+    const newTransaction = {id: transactionArray.length + 1, ...req.body}
+    transactionArray.push(newTransaction);
+    res.status(201).send(transactionArray[transactionArray.length - 1])
+})
+
+transaction.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const deleteTransaction = transactionArray.find(element => element.id === id)
+    
+    if (deleteTransaction !== -1) {
+        transactionArray.splice(deleteTransaction, 1)
+        res.redirect("/transactions")
+    } else  {
+        res.status(404).send({error: `Transaction with id: ${id} not found!`})
+    }
+})
+
+transaction.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const updateTransaction = transactionArray.findIndex(transaction => transaction.id === id)
+  
+    if (updateTransaction !== -1) {
+      transactionArray[updateTransaction] = req.body
+      res.status(200).json(transactionArray[updateTransaction])
+    } else {
+      res.status(404).send({error: `Transaction with id: ${id} not found!`})
+    }
+  })
+
 
 module.exports = transaction
