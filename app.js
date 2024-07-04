0192783c2a47c6309bc49c23c97a88334f044600
MarcusB86 <marcusb08@gmail.com>
2024-07-03 21:00:49 -0400

    Setup app-server-controllers-models-dependencies

---
+++ app.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const express = require("express");
+
+const cors = require('cors');
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json())
+
+app.use(cors());
+
+
+// // CONTROLLERS
+// const workoutsController = require("./controllers/workoutsController")
+
+
+// HEALTH CHECK ROUTE
+app.get("/", (req, res) => {
+  res.send("Welcome to my fly ass budgeting app")
+})
+
+// WHEN RECEIVING A REQUEST OR RESPONSE ON THE ROUTE `/workouts` WE WANT OUR APP TO USE THE `workoutsController`
+// app.use("/workouts", workoutsController)
+
+module.exports = app;
